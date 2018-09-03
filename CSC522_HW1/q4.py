1b942493c03e6d7384cec75db5fc1fed41c55d56
Aman Chauhan <achauha3@ncsu.edu>
2018-09-03 19:55:59 -0400

    Q4 initial solved problems. (a), (b), (c), (d), (e), (f)

---
+++ CSC522_HW1/q4.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+import random
+import math
+import sys
+import os
+
+
+def identity_matrix(dim):
+	return [[1 if j==i else 0 for j in range(dim)] for i in range(dim)]
+
+
+def column_manipulate(mat, col, val):
+	for row in mat:
+		row[col] = val
+	return mat
+
+
+def matrix_sum(mat):
+	cnt = 0
+	for row in mat:
+		for col in row:
+			cnt += col
+	return cnt
+
+
+def transpose(mat):
+	for i in range(len(mat)):
+		for j in range(i+1, len(mat[i])):
+			mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+	return mat
+
+
+def row_diagonal_sum(mat, row):
+	cnt=0
+	for i in range(len(mat)):
+		for j in range(len(mat[i])):
+			if i==j or i==row:
+				cnt += mat[i][j]
+	return cnt
+
+
+def gaussian_matrix(dim, mean, variance, dtype):
+	if dtype=="int":
+		return [[math.floor(random.gauss(mean, math.sqrt(variance))) for j in range(dim)] for i in range(dim)]
+	else:
+		return [[random.gauss(mean, math.sqrt(variance)) for j in range(dim)] for i in range(dim)]
+
+
+def main():
+	A = identity_matrix(5)
+	A = column_manipulate(A, 1, 3)
+	matrix_sum(A)
+	A = transpose(A)
+	row_diagonal_sum(A, 2)
+	B = gaussian_matrix(5, 5, 3, "int")
+
+
+if __name__=='__main__':
+	main()
